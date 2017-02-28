12776513503a3ebcac99c027e5618b6a268e8c9e
Alex Schultz <aschultz@redhat.com>
2017-02-28 08:18:26 -0700

    Remove rpc_backend check for amqp
    I7ccd995ef01c2d54427684718adba054260fdd52 removed the rpc_backend
    declaration for amqp so we need to stop checking for it in the unit
    tests.
    
    Change-Id: I9e4347c4ebf27d4b021bef15e9eff6b5e94d7e8e

--- spec/classes/octavia_init_spec.rb
+++ spec/classes/octavia_init_spec.rb
@@ -173,7 +173,6 @@ describe 'octavia' do
 
       context 'with default parameters' do
         it 'configures amqp' do
-          is_expected.to contain_octavia_config('DEFAULT/rpc_backend').with_value('amqp')
           is_expected.to contain_octavia_config('oslo_messaging_amqp/server_request_prefix').with_value('<SERVICE DEFAULT>')
           is_expected.to contain_octavia_config('oslo_messaging_amqp/broadcast_prefix').with_value('<SERVICE DEFAULT>')
           is_expected.to contain_octavia_config('oslo_messaging_amqp/group_request_prefix').with_value('<SERVICE DEFAULT>')
@@ -209,7 +208,6 @@ describe 'octavia' do
       end
 
       it 'configures amqp' do
-        is_expected.to contain_octavia_config('DEFAULT/rpc_backend').with_value('amqp')
         is_expected.to contain_octavia_config('DEFAULT/transport_url').with_value('amqp://amqp_user:password@localhost:5672')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/idle_timeout').with_value('60')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/trace').with_value('true')

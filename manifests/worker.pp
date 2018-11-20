c8c316f272aa480ffa1fe5e7eae1fd086ba1644e
Tobias Urdin <tobias.urdin@binero.se>
2018-11-20 15:55:00 +0100

    Fix octavia::controller redeclaration
    It's impossible to use this new class because you
    cannot use any services combined with and specify
    the parameters in octavia::controler since you get
    redeclaration issues.
    
    The class should only be included from octavia::worker
    if it doesn't exist to support the old use case.
    
    If you want to use this class it's required that you
    define octavia::controller *before* octavia::worker
    otherwise you'll end up with another redeclaration
    issue since we include the controller unless its
    defined before.
    
    Depends-On: https://review.openstack.org/#/c/618915/
    Change-Id: If5b4014d2129879199798d440ae604c03b01ab3a
    (cherry picked from commit 0ea4e10dd3e848c299b6b23b0e962f42ba76c327)

--- manifests/worker.pp
+++ manifests/worker.pp
@@ -114,13 +114,16 @@ class octavia::worker (
 ) inherits octavia::params {
 
   include ::octavia::deps
-  include ::octavia::controller
+
+  if !defined(Class['octavia::controller']) {
+    include ::octavia::controller
+  }
 
   if ($amp_flavor_id or $amp_image_tag or $amp_secgroup_list or $amp_boot_network_list or $loadbalancer_topology or $amphora_driver or
       $compute_driver or $network_driver or $amp_ssh_key_name or $enable_ssh_access) {
     warning('The amp_flavor_id, amp_image_tag, amp_secgroup_list, amp_boot_network_list, loadbalancer_topology, amphora_driver,
              compute_driver, network_driver, amp_ssh_key_name, enable_ssh_access, parameters are deprecated and have been moved to
-             octavia::controller class. Please set them there.')
+             octavia::controller class. Please set them there, you must set octavia::controller class before octavia::worker!')
   }
 
   validate_hash($nova_flavor_config)

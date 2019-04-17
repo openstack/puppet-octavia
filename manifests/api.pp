119f263ea73bb354e706f3d479be6313358df85b
Rabi Mishra <ramishra@redhat.com>
2019-04-17 18:40:35 +0530

    Revert "Convert provider drivers to a json string for config file"
    This partially reverts commit
    7ddad2622413ef11424eb1e4e9e52fe09cdce4b2. Now that it's passed
    as a string we don't need do validate_hash().
    
    Depends-On: https://review.openstack.org/653375
    Change-Id: I99cf987b8e40a6c79b699ff38631aa176f60cedb
    Related-Bug: #1825146

--- manifests/api.pp
+++ manifests/api.pp
@@ -127,12 +127,6 @@ class octavia::api (
     include ::octavia::db::sync
   }
 
-  if !is_service_default($provider_drivers) {
-    $provider_drivers_real = to_json($provider_drivers)
-  } else {
-    $provider_drivers_real = $provider_drivers
-  }
-
   octavia_config {
     'api_settings/bind_host':                      value => $host;
     'api_settings/bind_port':                      value => $port;
@@ -142,7 +136,7 @@ class octavia::api (
     'api_settings/api_v2_enabled':                 value => $api_v2_enabled;
     'api_settings/allow_tls_terminated_listeners': value => $allow_tls_terminated_listeners;
     'api_settings/default_provider_driver':        value => $default_provider_driver;
-    'api_settings/enabled_provider_drivers':       value => $provider_drivers_real;
+    'api_settings/enabled_provider_drivers':       value => $provider_drivers;
   }
 
 }

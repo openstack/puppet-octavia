a4f8833b726df2ebe4a84810b10262b0191405f5
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:11 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: Ie628b4900fd6614d64498d78e9f948e874d9e953
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -8,7 +8,7 @@
 #   (Optional) Url used to connect to database.
 #   Defaults to 'sqlite:////var/lib/octavia/octavia.sqlite'.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   (Optional) Timeout when db connections should be reaped.
 #   Defaults to $::os_service_default
 #
@@ -42,22 +42,36 @@
 #   before error is raised. Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class octavia::db (
-  $database_connection     = 'sqlite:////var/lib/octavia/octavia.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/octavia/octavia.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::octavia::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   $database_connection_real = pick($::octavia::database_connection, $database_connection)
-  $database_idle_timeout_real = pick($::octavia::database_idle_timeout, $database_idle_timeout)
+  $database_connection_recycle_time_real = pick($::octavia::database_idle_timeout, $database_idle_timeout,
+                                                $database_connection_recycle_time)
   $database_min_pool_size_real = pick($::octavia::database_min_pool_size, $database_min_pool_size)
   $database_max_pool_size_real = pick($::octavia::database_max_pool_size, $database_max_pool_size)
   $database_max_retries_real = pick($::octavia::database_max_retries, $database_max_retries)
@@ -70,15 +84,15 @@ class octavia::db (
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'octavia_config':
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
-    pool_timeout   => $database_pool_timeout_real,
-    db_max_retries => $database_db_max_retries_real,
+    connection              => $database_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout_real,
+    db_max_retries          => $database_db_max_retries_real,
   }
 
 }

daa3c1269f29ecb7aac47f77e41c3ee8b082e15f
Carlos Goncalves <cgoncalves@redhat.com>
2020-06-29 20:43:33 +0200

    Switch oslo.policy over to yaml
    Upstream switched oslo.policy from JSON to YAML format
    output in [1].
    
    [1] https://review.opendev.org/#/c/732453/
    
    Closes-Bug: #1885602
    
    Depends-On: Iec610053a9250cb78c2a17bfc2c197bf55d9df86
    Change-Id: Ieb6bc60eb2750a9f3f0539c2bd0176fabfebb30e

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the nova policy.json file
-#   Defaults to /etc/octavia/policy.json
+#   (Optional) Path to the octavia policy.yaml file
+#   Defaults to /etc/octavia/policy.yaml
 #
 class octavia::policy (
   $policies    = {},
-  $policy_path = '/etc/octavia/policy.json',
+  $policy_path = '/etc/octavia/policy.yaml',
 ) {
 
   include octavia::deps
@@ -34,9 +34,10 @@ class octavia::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::octavia::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::octavia::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)

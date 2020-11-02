8a8940c87e782ad437ff8e20d8f5dbfec8ec6109
ZhongShengping <chdzsp@163.com>
2020-11-02 14:42:04 +0800

    Deprecate allow_insecure_clients option
    The allow_insecure_clients has been deprecated[1].
    
    [1]https://review.opendev.org/#/c/417629/
    
    Change-Id: I42c519f74d6b53570a8aaf9f4423d0a41ff4f0e4
    Closes-Bug: #1902158

--- spec/classes/octavia_init_spec.rb
+++ spec/classes/octavia_init_spec.rb
@@ -196,7 +196,6 @@ describe 'octavia' do
         is_expected.to contain_octavia_config('oslo_messaging_amqp/ssl_cert_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/ssl_key_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/ssl_key_password').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_octavia_config('oslo_messaging_amqp/allow_insecure_clients').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/sasl_mechanisms').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/sasl_config_dir').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_octavia_config('oslo_messaging_amqp/sasl_config_name').with_value('<SERVICE DEFAULT>')

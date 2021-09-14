b86fe3e33eb9ed455757319aa992b4068191eecc
ZhongShengping <chdzsp@163.com>
2021-09-14 16:02:09 +0800

    Add watch_log_file option
    Add support for Using logging handler designed to watch file system.
    
    Change-Id: Iecc618707d243f9e56acf085c71bb03737bde70c
    Closes-Bug: #1943212

--- spec/classes/octavia_logging_spec.rb
+++ spec/classes/octavia_logging_spec.rb
@@ -30,6 +30,7 @@ describe 'octavia::logging' do
      :log_facility                   => 'LOG_FOO',
      :log_dir                        => '/var/log',
      :log_file                       => '/var/tmp/octavia_random.log',
+     :watch_log_file                 => true,
      :debug                          => true,
     }
   end
@@ -66,6 +67,7 @@ describe 'octavia::logging' do
         :syslog_log_facility => '<SERVICE DEFAULT>',
         :log_dir             => '/var/log/octavia',
         :log_file            => '/var/log/octavia/octavia.log',
+        :watch_log_file      => '<SERVICE DEFAULT>',
         :debug               => '<SERVICE DEFAULT>',
       )
     end
@@ -81,6 +83,7 @@ describe 'octavia::logging' do
         :syslog_log_facility => 'LOG_FOO',
         :log_dir             => '/var/log',
         :log_file            => '/var/tmp/octavia_random.log',
+        :watch_log_file      => true,
         :debug               => true,
       )
     end

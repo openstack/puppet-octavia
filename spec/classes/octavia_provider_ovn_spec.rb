openstack/puppet-octavia

commit 045272a8ce0c4e20f6ceef3bea9876fabf276524

Remove deprecated octavia::api::ovn_nb_connection
... which was deprecated during the Ussuri cycle[1] in favor of the new
independent octavia::provider::ovn class.

[1] 8777b5031c611b7a4dd9cc9afbf0e65a0069ccf1

Change-Id: I171da249fbe6e372f1b5ef57e7e6f641aa13ad6b

manifests/provider/ovn.pp
@@ -79,18 +79,11 @@ class octavia::provider::ovn (
     tag    => ['openstack', 'octavia-package'],
   }
 
-  # For backward compatibility
-  if $::octavia::api::ovn_nb_connection and !is_service_default($::octavia::api::ovn_nb_connection) {
-    $ovn_nb_connection_real = $::octavia::api::ovn_nb_connection
-  } else {
-    $ovn_nb_connection_real = $ovn_nb_connection
-  }
-
   # TODO(flaviof): We need to replace octavia_config with octavia_ovn_provider_config in the future.
   # For now, the config below uses octavia_config until we can figure out how to pass extra
   # configuration files to the api running as wsgi process.
   octavia_config {
-    'ovn/ovn_nb_connection':        value => $ovn_nb_connection_real;
+    'ovn/ovn_nb_connection':        value => $ovn_nb_connection;
     'ovn/ovn_nb_private_key':       value => $ovn_nb_private_key;
     'ovn/ovn_nb_certificate':       value => $ovn_nb_certificate;
     'ovn/ovn_nb_ca_cert':           value => $ovn_nb_ca_cert;

manifests/api.pp
@@ -80,12 +80,6 @@
 #   (optional) The interval healthcheck plugin should cache results, in seconds.
 #   Defaults to $::os_service_default
 #
-# DEPRECATED PARAMETERS
-#
-# [*ovn_nb_connection*]
-#   (optional) The connection string for the OVN_Northbound OVSDB.
-#   Defaults to undef
-#
 class octavia::api (
   $enabled                        = true,
   $manage_service                 = true,
@@ -105,8 +99,6 @@ class octavia::api (
   $pagination_max_limit           = $::os_service_default,
   $healthcheck_enabled            = $::os_service_default,
   $healthcheck_refresh_interval   = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $ovn_nb_connection              = undef
 ) inherits octavia::params {
 
   include octavia::deps
@@ -115,10 +107,6 @@ class octavia::api (
 
   if $auth_strategy == 'keystone' {
     include octavia::keystone::authtoken
-  }
-
-  if $ovn_nb_connection {
-      warning('The ovn_nb_connection parameter is deprecated from octavia::api. Use octavia::provider::ovn::ovn_nb_connection.')
   }
 
   package { 'octavia-api':

releasenotes/notes/remove-api-ovn_nb_connection-2d85f0ece3db7576.yaml
@@ -1,0 +1,4 @@
+---
+upgrade:
+  - |
+    The ``octavia::api::ovn_nb_connection`` parameter has been removed.

commit 91031e6d0068fefdaa7fc3301452f2f0827b9118

ovn: Accept arrays for ovn_*_connection parameters
These parameters are regarded as StrOpt, but can accept comma-seprated
strings.

Conflicts:
	manifests/provider/ovn.pp

Resolved conflict caused by 045272a8ce0c4e20f6ceef3bea9876fabf276524

Change-Id: I725d7c167886ae799663bbe3c033fb99a81f9711
(cherry picked from commit cf154d7172dc928b37f5453c4e7192a4b7abc6a5)
(cherry picked from commit 2d2404649e201525d21bf39d751dda70a4f8162a)

manifests/provider/ovn.pp
@@ -90,11 +90,11 @@ class octavia::provider::ovn (
   # For now, the config below uses octavia_config until we can figure out how to pass extra
   # configuration files to the api running as wsgi process.
   octavia_config {
-    'ovn/ovn_nb_connection':        value => $ovn_nb_connection_real;
+    'ovn/ovn_nb_connection':        value => join(any2array($ovn_nb_connection_real), ',');
     'ovn/ovn_nb_private_key':       value => $ovn_nb_private_key;
     'ovn/ovn_nb_certificate':       value => $ovn_nb_certificate;
     'ovn/ovn_nb_ca_cert':           value => $ovn_nb_ca_cert;
-    'ovn/ovn_sb_connection':        value => $ovn_sb_connection;
+    'ovn/ovn_sb_connection':        value => join(any2array($ovn_sb_connection), ',');
     'ovn/ovn_sb_private_key':       value => $ovn_sb_private_key;
     'ovn/ovn_sb_certificate':       value => $ovn_sb_certificate;
     'ovn/ovn_sb_ca_cert':           value => $ovn_sb_ca_cert;

spec/classes/octavia_provider_ovn_spec.rb
@@ -57,6 +57,22 @@ describe 'octavia::provider::ovn' do
       it { is_expected.to contain_octavia_config('ovn/ovsdb_retry_max_interval').with_value(180) }
       it { is_expected.to contain_octavia_config('ovn/ovsdb_probe_interval').with_value(60000) }
     end
+
+    context 'with specific parameters' do
+      before do
+        params.merge!({
+          :ovn_nb_connection => ['tcp:192.0.2.11:6641', 'tcp:192.0.2.12:6641'],
+          :ovn_sb_connection => ['tcp:192.0.2.11:6642', 'tcp:192.0.2.12:6642'],
+        })
+      end
+
+      it { is_expected.to contain_octavia_config('ovn/ovn_nb_connection').with_value(
+        'tcp:192.0.2.11:6641,tcp:192.0.2.12:6641'
+      ) }
+      it { is_expected.to contain_octavia_config('ovn/ovn_sb_connection').with_value(
+        'tcp:192.0.2.11:6642,tcp:192.0.2.12:6642'
+      ) }
+    end
   end
 
   on_supported_os({
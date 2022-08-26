49b37d5fd6931e98b000ccf07c77cb9d2c4847f9
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 15:29:14 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: I1c4a1af57f090cfd0d2edeca897ee23e50203d12

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -69,15 +69,33 @@
 #
 # [*access_log_file*]
 #   (Optional) The log file name for the virtualhost.
-#   Defaults to undef
+#   Defaults to undef.
+#
+# [*access_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends access logs for
+#   the virtualhost.
+#   Defaults to undef.
+#
+# [*access_log_syslog*]
+#   (Optional) Sends the virtualhost access log messages to syslog.
+#   Defaults to undef.
 #
 # [*access_log_format*]
 #   (Optional) The log format for the virtualhost.
-#   Defaults to undef
+#   Defaults to undef.
 #
 # [*error_log_file*]
 #   (Optional) The error log file name for the virtualhost.
-#   Defaults to undef
+#   Defaults to undef.
+#
+# [*error_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends error logs for
+#   the virtualhost.
+#   Defaults to undef.
+#
+# [*error_log_syslog*]
+#   (Optional) Sends the virtualhost error log messages to syslog.
+#   Defaults to undef.
 #
 # [*custom_wsgi_process_options*]
 #   (Optional) gives you the opportunity to add custom process options or to
@@ -131,8 +149,12 @@ class octavia::wsgi::apache (
   $threads                     = 1,
   $priority                    = 10,
   $access_log_file             = undef,
+  $access_log_pipe             = undef,
+  $access_log_syslog           = undef,
   $access_log_format           = undef,
   $error_log_file              = undef,
+  $error_log_pipe              = undef,
+  $error_log_syslog            = undef,
   $custom_wsgi_process_options = {},
   $wsgi_script_dir             = $::octavia::params::octavia_wsgi_script_path,
   $wsgi_script_source          = $::octavia::params::octavia_wsgi_script_source,
@@ -174,7 +196,11 @@ class octavia::wsgi::apache (
     request_headers             => $request_headers,
     custom_wsgi_process_options => $custom_wsgi_process_options,
     access_log_file             => $access_log_file,
+    access_log_pipe             => $access_log_pipe,
+    access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
+    error_log_pipe              => $error_log_pipe,
+    error_log_syslog            => $error_log_syslog,
   }
 }

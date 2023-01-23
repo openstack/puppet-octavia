7df58f765777da1d7b95c373545cc67b6f16d44b
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:39:56 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: I2c1e548212e0b130b2e9f67b3008e5f8537ea76f

--- spec/classes/octavia_policy_spec.rb
+++ spec/classes/octavia_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'octavia::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/octavia/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/octavia/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'octavia::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/octavia/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/octavia/policy.d',
         )
       end

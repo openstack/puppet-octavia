11fd51e93de4df0f14d7b8baa41665ed2363bbbd
Takashi Kajinami <tkajinam@redhat.com>
2023-09-11 12:31:59 +0900

    Do not restart services after policy file changes
    The oslo.policy library has implementations to detect change in policy
    rules and reload the new rules without service restart.
    
    Change-Id: I92b0e0098914fe52f5605887721516d6f9746fee

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -30,7 +30,7 @@ class octavia::deps {
   # policy config should occur in the config block also.
   Anchor['octavia::config::begin']
   -> Openstacklib::Policy<| tag == 'octavia' |>
-  ~> Anchor['octavia::config::end']
+  -> Anchor['octavia::config::end']
 
   # On any uwsgi config change, we must restart Octavia API.
   Anchor['octavia::config::begin']

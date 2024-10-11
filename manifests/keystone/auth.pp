57f3352a64da436333f65403172d0cc5ae7ce811
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-10-12 01:30:05 +0900

    Expose configure_service
    This parameter has been implemented in the underlying defined resource
    type but was not configurable.
    
    Change-Id: I8e9caebde4d7d664f17d54b57370e46e0447d04b

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -43,6 +43,10 @@
 #   (Optional) Should the admin role be configured for the service user?
 #   Defaults to true.
 #
+# [*configure_service*]
+#   (Optional) Should the service be configurd?
+#   Defaults to True
+#
 # [*service_type*]
 #   (Optional) Type of service.
 #   Defaults to 'load-balancer'.
@@ -85,6 +89,7 @@ class octavia::keystone::auth (
   Boolean $configure_endpoint             = true,
   Boolean $configure_user                 = true,
   Boolean $configure_user_role            = true,
+  Boolean $configure_service              = true,
   String[1] $service_description          = 'OpenStack Load Balancing Service',
   String[1] $service_name                 = 'octavia',
   String[1] $service_type                 = 'load-balancer',
@@ -102,6 +107,7 @@ class octavia::keystone::auth (
     configure_user      => $configure_user,
     configure_user_role => $configure_user_role,
     configure_endpoint  => $configure_endpoint,
+    configure_service   => $configure_service,
     service_name        => $service_name,
     service_type        => $service_type,
     service_description => $service_description,

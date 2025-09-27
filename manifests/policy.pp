e5aa65a6307b3d8d227c1b434c9d4c368956f8b2
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-27 23:49:38 +0900

    Import base parameter types for ::policy
    Import the parameter types from openstacklib::policy so that parameter
    types are validated at module level, instead of internal resource
    call.
    
    Also remove the tag which is no longer necessary after dependency
    refactoring.
    
    Change-Id: I44f80c0fdcd3333df0548dfb13e474ef26d8c617
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -46,13 +46,13 @@
 #    Defaults to false.
 #
 class octavia::policy (
-  $enforce_scope        = $facts['os_service_default'],
-  $enforce_new_defaults = $facts['os_service_default'],
-  Hash $policies        = {},
-  $policy_path          = '/etc/octavia/policy.yaml',
-  $policy_default_rule  = $facts['os_service_default'],
-  $policy_dirs          = $facts['os_service_default'],
-  Boolean $purge_config = false,
+  $enforce_scope                    = $facts['os_service_default'],
+  $enforce_new_defaults             = $facts['os_service_default'],
+  Openstacklib::Policies $policies  = {},
+  Stdlib::Absolutepath $policy_path = '/etc/octavia/policy.yaml',
+  $policy_default_rule              = $facts['os_service_default'],
+  $policy_dirs                      = $facts['os_service_default'],
+  Boolean $purge_config             = false,
 ) {
   include octavia::deps
   include octavia::params
@@ -64,7 +64,6 @@ class octavia::policy (
     file_group   => $octavia::params::group,
     file_format  => 'yaml',
     purge_config => $purge_config,
-    tag          => 'octavia',
   }
 
   create_resources('openstacklib::policy', { $policy_path => $policy_parameters })
